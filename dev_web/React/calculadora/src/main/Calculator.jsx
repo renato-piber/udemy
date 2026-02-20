0b9ec48b63db515270db383d4e973b0ee21c558f
rena-to <ifcrenato@gmail.com>
2026-02-20 15:16:23 -0300

    terminei a calculadora

--- dev_web/React/calculadora/src/main/Calculator.jsx
+++ dev_web/React/calculadora/src/main/Calculator.jsx
@@ -12,19 +12,64 @@ const initialState = {
 }
 
 const Calculator = () => {
-
     const [state, setState] = useState(initialState)
 
     function clearMemory() {
-       setState(initialState) 
+        setState(initialState)
     }
 
     function setOperation(operation) {
-        console.log(operation)
+        if (state.current === 0) {
+            setState(state => ({ ...state, operation, current: 1, clearDisplay: true }))
+        } else {
+            const equals = operation === "="
+            const currentOperation = state.operation
+
+            const values = [...state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory()
+                    return
+                }
+            } catch (e) {
+                values[0] = state.values[0]
+            }
+
+            values[1] = 0
+
+            setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     function addDigit(n) {
-        console.log(n)
+        if (n === "." && state.displayValue.includes(".")) return /* evitar dois pontos */
+        const clearDisplay = state.displayValue === "0"
+            || state.clearDisplay /* aqui permite n acumular os zeros e zerar o display */
+
+        const currentValue = clearDisplay ? "" : state.displayValue // aqui acumula os numeros e reseta 
+
+        const displayValue = currentValue + n /* novo valor do display */
+
+        setState(state => (
+            { ...state, displayValue, clearDisplay: false }
+        ))
+
+        // atualiza o values
+        if (n !== ".") {
+
+            const i = state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...state.values]
+            values[i] = newValue
+            setState(state => ({ ...state, values }))
+        }
     }
 
     return (
@@ -50,4 +95,5 @@ const Calculator = () => {
         </div>
     )
 }
+
 export default Calculator
