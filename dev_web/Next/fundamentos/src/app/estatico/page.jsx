938a90bb3d1b9cc8047ff8afa1bc0b759ba491d1
Renato Piber <renatopiber@hotmail.com>
2026-07-15 19:58:51 -0300

    iniciando o crud com next

--- dev_web/Next/fundamentos/src/app/estatico/page.jsx
+++ dev_web/Next/fundamentos/src/app/estatico/page.jsx
@@ -1,9 +1,47 @@
+
 import Layout from "../../components/Layout"
 
-export default function Estatico() {
-    return (
-        <Layout>
+// versao do pages router
+// export function getStaticProps() {
+//     return {
+//         props: {
+//             numero: Math.random()
+//         }
+//     }
+// }
 
+// pre-renderizacao estatica (SSG)-> o next armazena no cache e vai servindo esses dadsos
+// agora o next decide automaticamente qual tipo de renderizacao usar
+// n depende mais de funcoes, mas sim dos componentes (onde serao usadas e qnd?)
+async function getDados() {
+    // fetch....
+    return {
+        numero: Math.random()
+    }
+}
+
+/*
+
+SSR -> sempre busca no servidor
+const res = await fetch(url, {
+    cache: "no-store"
+})
+
+ISR -> atualiza de tempo em tempos
+const res = await fetch(url, {
+    next: {
+        revalidate: 60
+    }
+})
+
+*/
+
+export default async function Estatico() {
+    const dados = await getDados();
+
+    return (
+        <Layout titulo="Conteúdo Estático">
+            <div>{dados.numero}</div>
         </Layout>
     )
 }
---
+++ dev_web/Next/next-crud/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <span className="text-4xl">Texto</span>
+    </div>
+  );
+}
